a77eed95bedc64ed9c0919b9861d7a689a59417c
seoyoung030121 <167770951+dangerouseoyoung@users.noreply.github.com>
2025-04-21 11:18:36 +0900

    [fix] config 수정

--- src/main/java/teamplace/pixi/contoller/HomeController.java
+++ src/main/java/teamplace/pixi/contoller/HomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @GetMapping("/api/hello")
+public class HomeController {
+    @GetMapping("/")
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello, World!");
     }
