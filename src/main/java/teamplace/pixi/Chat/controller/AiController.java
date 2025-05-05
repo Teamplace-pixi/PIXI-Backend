a957263830db3ca126ba23c2ffb37e285248b3f0
limsunwoo <mario0111@kau.kr>
2025-05-06 01:03:57 +0900

    Chat Update

--- src/main/java/teamplace/pixi/Chat/controller/AiController.java
+++ src/main/java/teamplace/pixi/Chat/controller/AiController.java
@@ -7,17 +7,22 @@ import teamplace.pixi.Chat.dto.AiChatRequest;
 import teamplace.pixi.Chat.dto.AiChatResponse;
 import teamplace.pixi.Chat.service.AiService;
 
+//RestApi의 엔드포인트
 @RestController
-@RequestMapping("/api/chat")
+// /api/chat 으로 Post할 수 있게 경로 설정
+@RequestMapping("/ai/chat")
 @RequiredArgsConstructor
 public class AiController {
 
     private final AiService aiService;
 
+    //Post 요청만 받음.
     @PostMapping
     public ResponseEntity<AiChatResponse> chat(@RequestBody AiChatRequest request) {
+        // 클라이언트가 보낸 JSON 데이터를 Java 객체로 변환해서 받음.
         try {
             AiChatResponse response = aiService.sendToFastApi(request);
+            //받은 데이터를 서비스로 전달
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             return ResponseEntity.internalServerError()
