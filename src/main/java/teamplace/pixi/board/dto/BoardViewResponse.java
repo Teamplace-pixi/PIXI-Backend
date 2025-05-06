1e4584b83e6c9a5584a9485a51533745de14bec6
seoyoung030121 <167770951+dangerouseoyoung@users.noreply.github.com>
2025-05-06 19:10:41 +0900

    fix: 구해요글을 조회할 때 현재 로그인한 사용자의 권한 조회

--- src/main/java/teamplace/pixi/board/dto/BoardViewResponse.java
+++ src/main/java/teamplace/pixi/board/dto/BoardViewResponse.java
@@ -2,6 +2,7 @@ package teamplace.pixi.board.dto;
 
 import lombok.Getter;
 import teamplace.pixi.board.domain.Board;
+import teamplace.pixi.user.domain.User;
 
 import java.time.format.DateTimeFormatter;
 
@@ -14,8 +15,9 @@ public class BoardViewResponse {
     private final String boardDate; // "yyyy-MM-dd"
     private final String deviceName;
     private final String nickname;
+    private final int rollId;
 
-    public BoardViewResponse(Board board) {
+    public BoardViewResponse(Board board, User currentUser) {
         this.boardTitle = board.getBoardTitle();
         this.boardContent = board.getBoardContent();
         this.boardLoc = board.getBoardLoc();
@@ -23,5 +25,6 @@ public class BoardViewResponse {
         this.boardDate = board.getBoardDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.deviceName = board.getDevice().getDeviceName();
         this.nickname = board.getUser().getNickname();
+        this.rollId = currentUser.getRollId();
     }
 }
