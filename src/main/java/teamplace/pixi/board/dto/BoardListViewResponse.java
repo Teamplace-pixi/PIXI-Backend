3efb9a1074d818d9a3dd1de2be4bcada1406c3e7
seoyoung030121 <167770951+dangerouseoyoung@users.noreply.github.com>
2025-05-27 21:07:31 +0900

    fix: 구해요 본문에 모집 상태가 주어지게 변경
    fix: 구해요 본문에 모집 상태가 주어지게 변경

--- src/main/java/teamplace/pixi/board/dto/BoardListViewResponse.java
+++ src/main/java/teamplace/pixi/board/dto/BoardListViewResponse.java
@@ -11,6 +11,7 @@ public class BoardListViewResponse {
     private final String boardTitle;
     private final int boardCost;
     private final String boardDate;
+    private final String boardStatus;
     private final String deviceBrand;
     private final int deviceType;
 
@@ -19,6 +20,7 @@ public class BoardListViewResponse {
         this.boardTitle = board.getBoardTitle();
         this.boardCost = board.getBoardCost();
         this.boardDate = board.getBoardDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        this.boardStatus = board.getBoardStatus();
         this.deviceBrand = board.getDevice().getDeviceBrand();
         this.deviceType = board.getDevice().getDeviceType();
     }
