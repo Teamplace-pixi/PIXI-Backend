ba2726abcb54a6b29a14854a04045e12ebaa655c
dangerouseoyoung <167770951+dangerouseoyoung@users.noreply.github.com>
2025-05-31 03:47:24 +0900

    feat: 1개월 구독 모델
    1개월 구독 기간을 갱신하고 상태를 변경

--- src/main/java/teamplace/pixi/user/repository/UserRepository.java
+++ src/main/java/teamplace/pixi/user/repository/UserRepository.java
@@ -3,9 +3,12 @@ package teamplace.pixi.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamplace.pixi.user.domain.User;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
+    List<User> findAllBySubscriptionEndDateBefore(LocalDateTime dateTime);
 }
