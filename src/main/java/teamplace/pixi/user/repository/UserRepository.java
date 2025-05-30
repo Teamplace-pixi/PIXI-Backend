63fcee80d2e8fb4234591049b9f2080362289cdd
dangerouseoyoung <167770951+dangerouseoyoung@users.noreply.github.com>
2025-05-31 03:50:07 +0900

    fix: 구독기간 체크를 위한 레포지토리
    fix: 구독기간 체크를 위한 레포지토리

--- src/main/java/teamplace/pixi/user/repository/UserRepository.java
+++ src/main/java/teamplace/pixi/user/repository/UserRepository.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
+    List<User> findAllBySubscriptionEndDateBefore(LocalDateTime dateTime);
     @Query("SELECT u.rollId FROM User u WHERE u.userId = :userId")
     Integer findRollIdByUserId(@Param("userId") Long userId);
 }
